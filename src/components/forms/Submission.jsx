39c0d260750efa60f9c89ca087d64c17b47113e6
viddie <viddie@hotmail.de>
2026-09-06 12:41:06 +0200

    Always show is_obsolete checkbox

--- src/components/forms/Submission.jsx
+++ src/components/forms/Submission.jsx
@@ -54,11 +54,7 @@ import { CreateAnyButton } from "../../pages/manage/Challenges";
 import { CustomModal, ModalButtons, useModal } from "../../hooks/useModal";
 import { ChallengeDetailsListWrapper, CollectiblesInfoBox, NoteDisclaimer } from "../../pages/Challenge";
 import { CharsCountLabel } from "../../pages/Suggestions";
-import {
-  durationToSeconds,
-  getNavigatorLanguage,
-  secondsToDuration,
-} from "../../util/data_util";
+import { durationToSeconds, getNavigatorLanguage, secondsToDuration } from "../../util/data_util";
 
 export function FormSubmissionWrapper({ id, onSave, ...props }) {
   const { t: t_g } = useTranslation(undefined, { keyPrefix: "general" });
@@ -275,34 +271,34 @@ export function FormSubmission({ submission, onSave, ...props }) {
             )}
           />
           {isHelper && submission.is_verified !== null && (
-            <>
-              <Controller
-                control={form.control}
-                name="is_verified"
-                defaultValue={submission.is_verified}
-                render={({ field }) => (
-                  <FormControlLabel
-                    onChange={field.onChange}
-                    label={t("is_verified")}
-                    checked={field.value}
-                    control={<Checkbox />}
-                  />
-                )}
-              />
-              <Controller
-                control={form.control}
-                name="is_obsolete"
-                defaultValue={submission.is_obsolete}
-                render={({ field }) => (
-                  <FormControlLabel
-                    onChange={field.onChange}
-                    label={t("is_obsolete")}
-                    checked={field.value}
-                    control={<Checkbox />}
-                  />
-                )}
-              />
-            </>
+            <Controller
+              control={form.control}
+              name="is_verified"
+              defaultValue={submission.is_verified}
+              render={({ field }) => (
+                <FormControlLabel
+                  onChange={field.onChange}
+                  label={t("is_verified")}
+                  checked={field.value}
+                  control={<Checkbox />}
+                />
+              )}
+            />
+          )}
+          {isHelper && (
+            <Controller
+              control={form.control}
+              name="is_obsolete"
+              defaultValue={submission.is_obsolete}
+              render={({ field }) => (
+                <FormControlLabel
+                  onChange={field.onChange}
+                  label={t("is_obsolete")}
+                  checked={field.value}
+                  control={<Checkbox />}
+                />
+              )}
+            />
           )}
           {isHelper && (
             <>
